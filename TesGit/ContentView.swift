37dc34c21baffe8ed2b5465a0ca5479ef1989efa
hadiakbar96 <hadiakbar96@gmail.com>
2026-06-04 09:26:29 +0700

    Initial Commit

---
+++ TesGit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TesGit
+//
+//  Created by Hadi Alfian Akbar on 04/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TesGit/TesGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesGitApp.swift
+//  TesGit
+//
+//  Created by Hadi Alfian Akbar on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TesGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
